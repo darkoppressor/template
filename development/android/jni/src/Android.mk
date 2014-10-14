c6be54bcce2e8225323e89ad0a7ecfda9f002c91
Kevin Wells <DarkOppressor@gmail.com>
2014-10-13 23:44:14 -0500

    Added Linux x86_64 build platform. Created and setup for android-prep program

--- development/android/jni/src/Android.mk
+++ development/android/jni/src/Android.mk
@@ -7,12 +7,14 @@ LOCAL_MODULE := main
 SDL_PATH := ../SDL2
 SDL_IMAGE_PATH := ../SDL2_image
 SDL_MIXER_PATH := ../SDL2_mixer
+RAKNET_PATH := ../RakNet
+BOOST_PATH := ../boost
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
 $(LOCAL_PATH)/$(SDL_IMAGE_PATH) \
 $(LOCAL_PATH)/$(SDL_MIXER_PATH) \
-C:/Development/c++/boost \
-C:/Development/c++/android/raknet
+$(LOCAL_PATH)/$(BOOST_PATH) \
+$(LOCAL_PATH)/$(RAKNET_PATH)/..
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
@@ -74,8 +76,10 @@ LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
 ../../../../window_scrolling_buttons.cpp \
 ../../../../world.cpp
 
+
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer RakNet
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
 include $(BUILD_SHARED_LIBRARY)
+
--- development/android/jni/src/create-source-file-list.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-SETLOCAL ENABLEDELAYEDEXPANSION
-
-cd ../../../..
-
-for /f "tokens=*" %%f in ('dir /b *.cpp') do (
-	echo ../../../../%%f \>>development/android/jni/src/source_file_list
-)
