7c5a6a017701840580dab38bcf8425a0235c436a
Kevin Wells <DarkOppressor@gmail.com>
2015-03-11 02:10:09 -0500

    Updated engine version

--- version.h
+++ version.h
@@ -6,8 +6,8 @@
 namespace Version{
 	//Build date
 	static const char YEAR[]="2015";
-	static const char MONTH[]="02";
-	static const char DAY[]="13";
+	static const char MONTH[]="03";
+	static const char DAY[]="11";
 
 	//Version
 	static const int MAJOR=0;
@@ -18,10 +18,10 @@ namespace Version{
 	static const char STATUS[]="alpha";
 
 	//Engine version
-	static const std::string ENGINE_VERSION="1.2.4";
+	static const std::string ENGINE_VERSION="1.2.5";
 
 	//Engine date
-	static const std::string ENGINE_DATE="2015-02-13";
+	static const std::string ENGINE_DATE="2015-03-11";
 }
 
 #endif
