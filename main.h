e72113c142d0f12103e8818fc8f572563531fc98
Kevin Wells <darkoppressor@gmail.com>
2015-09-26 06:39:31 -0500

    Move main startup code and game loop to engine

--- main.h
+++ main.h
@@ -5,13 +5,6 @@
 #ifndef main_h
 #define main_h
 
-#include <SDL.h>
-
-void game_loop();
-
-//Handle Android/iOS events
-int handle_app_events(void* userdata,SDL_Event* event);
-
 //Apparently, SDL likes main() to take these arguments
 int main(int argc,char* args[]);
 
