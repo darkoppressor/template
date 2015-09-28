415e4260c2099dd62452a039c7be82dfa5a2966f
Kevin Wells <darkoppressor@gmail.com>
2015-09-28 01:39:39 -0500

    Fix initial version series

--- version.cpp
+++ version.cpp
@@ -35,5 +35,5 @@ string Engine_Version::get_status(){
 }
 
 void Engine_Version::populate_version_series(vector<Version_Series>& version_series){
-    version_series.push_back(Version_Series("0.0.0","0.0.1"));
+    version_series.push_back(Version_Series("0.0.1","0.0.1"));
 }
