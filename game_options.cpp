8a91287ec5913dd40b1e18d75bdb13c020c4e20f
Kevin Wells <darkoppressor@gmail.com>
2015-10-17 14:50:54 -0500

    Add initial value to static variable

--- game_options.cpp
+++ game_options.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-///int Game_Options::example_option;
+///int Game_Options::example_option=0;
 
 bool Game_Options::get_option(string name,string& value){
     /**if(name=="cl_example_option"){
